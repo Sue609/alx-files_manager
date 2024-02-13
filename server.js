18845311a8641d692b28a47f262e627c9cf07a4d
Sue609 <susankamu609@gmail.com>
2024-02-13 12:38:24 +0300

    create the Express server:

---
+++ server.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const express = require('express');
+const routes = require('./routes/index');
+
+const app = express();
+
+app.use('/', routes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
