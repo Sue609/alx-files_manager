763abc12f5390d1c06b742ba387d7c4e3f8b9c31
Sue609 <susankamu609@gmail.com>
2024-02-13 12:46:28 +0300

    create a file AppController.js that contains the definition of the 2 endpoints:

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,20 +1,30 @@
+#!/usr/bin/node
+
 const dbClient = require('../utils/db');
 const redisClient = require('../utils/redis');
 
 class AppController {
-    static getStatus(req, res) {
-        if (redisClient.isAlive() && dbClient.isAlive()) {
-            res.json({ redis: true, db: true });
-            res.end();
-        }
+  static async getStatus(req, res) {
+    try {
+      const redisStatus = await redisClient.isAlive();
+      const dbStatus = await dbClient.isAlive();
+      
+      res.status(200).json({ redis: redisStatus, db: dbStatus });
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
     }
+  }
 
-    static async getStats(req, res) {
-        const user = await dbClient.nbUsers();
-        const files = await dbClient.nbFiles();
-        res.json({ users, files });
-        res.end();
+  static async getStats(req, res) {
+    try {
+      const usersCount = await dbClient.nbUsers();
+      const filesCount = await dbClient.nbFiles();
+      
+      res.status(200).json({ users: usersCount, files: filesCount });
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
     }
+  }
 }
 
 module.exports = AppController;
