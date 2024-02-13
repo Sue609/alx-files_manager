1ef838f849ad9ce23cfc408214a20c7f1b0f91af
Sue609 <susankamu609@gmail.com>
2024-02-13 12:40:12 +0300

    create a file AppController.js that contains the definition of the 2 endpoints:

---
+++ controllers/AppController.js
@@ -0,0 +1,20 @@
+const dbClient = require('../utils/db');
+const redisClient = require('../utils/redis');
+
+class AppController {
+    static getStatus(req, res) {
+        if (redisClient.isAlive() && dbClient.isAlive()) {
+            res.json({ redis: true, db: true });
+            res.end();
+        }
+    }
+
+    static async getStats(req, res) {
+        const user = await dbClient.nbUsers();
+        const files = await dbClient.nbFiles();
+        res.json({ users, files });
+        res.end();
+    }
+}
+
+module.exports = AppController;
